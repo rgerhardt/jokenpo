c3fb00233b922b1a8311f743a4971e312983d023
Ricardo Gerhardt <ric.gerhardt@gmail.com>
2018-09-03 18:34:44 -0300

    add spock and lizard

--- src/main/java/com/rgerhardt/jokenpo/Hand.java
+++ src/main/java/com/rgerhardt/jokenpo/Hand.java
@@ -4,19 +4,31 @@ public enum  Hand {
     ROCK {
         @Override
         public boolean beats(Hand other) {
-            return other == SCISSORS;
+            return other == SCISSORS || other == LIZARD;
         }
     },
     PAPER {
         @Override
         public boolean beats(Hand other) {
-            return other == ROCK;
+            return other == ROCK || other == SPOCK;
         }
     },
     SCISSORS {
         @Override
         public boolean beats(Hand other) {
-            return other == PAPER;
+            return other == PAPER || other == LIZARD;
+        }
+    },
+    LIZARD {
+        @Override
+        public boolean beats(Hand other) {
+            return other == SPOCK || other == PAPER;
+        }
+    },
+    SPOCK {
+        @Override
+        public boolean beats(Hand other) {
+            return other == SCISSORS || other == ROCK;
         }
     };
 
--- src/test/java/com/rgerhardt/jokenpo/JudgeTest.java
+++ src/test/java/com/rgerhardt/jokenpo/JudgeTest.java
@@ -24,6 +24,20 @@ public class JudgeTest {
             "PAPER, SCISSORS, PLAYER2",
             "ROCK, SCISSORS, PLAYER1",
             "SCISSORS, ROCK, PLAYER2",
+            "ROCK, LIZARD, PLAYER1",
+            "LIZARD, ROCK, PLAYER2",
+            "LIZARD, SPOCK, PLAYER1",
+            "SPOCK, LIZARD, PLAYER2",
+            "SPOCK, SCISSORS, PLAYER1",
+            "SCISSORS, SPOCK, PLAYER2",
+            "SCISSORS, LIZARD, PLAYER1",
+            "LIZARD, SCISSORS, PLAYER2",
+            "LIZARD, PAPER, PLAYER1",
+            "PAPER, LIZARD, PLAYER2",
+            "PAPER, SPOCK, PLAYER1",
+            "SPOCK, PAPER, PLAYER2",
+            "SPOCK, ROCK, PLAYER1",
+            "ROCK, SPOCK, PLAYER2",
     })
     public void testEvaluate_DifferentHands_returnWinner(Hand player1Hand, Hand player2Hand, MatchResult expectedResult) {
         MatchResult result = judge.evaluate(player1Hand, player2Hand);
@@ -35,6 +49,8 @@ public class JudgeTest {
             "ROCK, ROCK, DRAW",
             "PAPER, PAPER, DRAW",
             "SCISSORS, SCISSORS, DRAW",
+            "LIZARD, LIZARD, DRAW",
+            "SPOCK, SPOCK, DRAW"
     })
     public void testEvaluate_SameHands_returnDraw(Hand player1Hand, Hand player2Hand, MatchResult expectedResult) {
         MatchResult result = judge.evaluate(player1Hand, player2Hand);
