99ec49cb26e285cf31f1d53c17e4aa7fc17ea6e9
Ricardo Gerhardt <ric.gerhardt@gmail.com>
2018-09-03 18:10:04 -0300

    use parameterized tests instead of multiple methods

--- src/test/java/com/rgerhardt/jokenpo/JudgeTest.java
+++ src/test/java/com/rgerhardt/jokenpo/JudgeTest.java
@@ -2,7 +2,8 @@ package com.rgerhardt.jokenpo;
 
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,49 +16,29 @@ public class JudgeTest {
         judge = new Judge();
     }
 
-    @Test
-    public void returnDrawIfHandsAreEqual() {
-        for(Hand hand: Hand.values()) {
-            assertEquals(MatchResult.DRAW, judge.evaluate(hand, hand));
-        }
+    @ParameterizedTest(name = "Hand1 {0} Hand2 {1} Winner {2}")
+    @CsvSource({
+            "PAPER, ROCK, PLAYER1",
+            "ROCK, PAPER, PLAYER2",
+            "SCISSORS, PAPER, PLAYER1",
+            "PAPER, SCISSORS, PLAYER2",
+            "ROCK, SCISSORS, PLAYER1",
+            "SCISSORS, ROCK, PLAYER2",
+    })
+    public void testEvaluate_DifferentHands_returnWinner(Hand player1Hand, Hand player2Hand, MatchResult expectedResult) {
+        MatchResult result = judge.evaluate(player1Hand, player2Hand);
+        assertEquals(expectedResult, result);
     }
 
-    @Test
-    public void returnPlayer1IfHandsAreScissorsAndPaper() {
-        MatchResult result = judge.evaluate(Hand.SCISSORS, Hand.PAPER);
-        assertEquals(MatchResult.PLAYER1, result);
+    @ParameterizedTest(name = "Hand1 {0} Hand2 {1} Result {2}")
+    @CsvSource({
+            "ROCK, ROCK, DRAW",
+            "PAPER, PAPER, DRAW",
+            "SCISSORS, SCISSORS, DRAW",
+    })
+    public void testEvaluate_SameHands_returnDraw(Hand player1Hand, Hand player2Hand, MatchResult expectedResult) {
+        MatchResult result = judge.evaluate(player1Hand, player2Hand);
+        assertEquals(expectedResult, result);
     }
-
-    @Test
-    public void returnPlayer2IfHandsArePaperAndScissors() {
-        MatchResult result = judge.evaluate(Hand.PAPER,Hand.SCISSORS);
-        assertEquals(MatchResult.PLAYER2, result);
-    }
-
-    @Test
-    public void returnPlayer1IfHandsArePaperAndRock() {
-        MatchResult result = judge.evaluate(Hand.PAPER, Hand.ROCK);
-        assertEquals(MatchResult.PLAYER1, result);
-    }
-
-    @Test
-    public void returnPlayer2IfHandsAreRockAndPaper() {
-        MatchResult result = judge.evaluate(Hand.ROCK, Hand.PAPER);
-        assertEquals(MatchResult.PLAYER2, result);
-    }
-
-    @Test
-    public void returnPlayer1IfHandsAreRockAndScissors() {
-        MatchResult result = judge.evaluate(Hand.ROCK, Hand.SCISSORS);
-        assertEquals(MatchResult.PLAYER1, result);
-    }
-
-    @Test
-    public void returnPlayer2IfHandsAreScissorsAndRock() {
-        MatchResult result = judge.evaluate(Hand.SCISSORS, Hand.ROCK);
-        assertEquals(MatchResult.PLAYER2, result);
-    }
-
-
 
 }
